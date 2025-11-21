b892bf0906d6f206199eea7d0ceec1c958c8ed81
Hugh Nimmo-Smith <hughns@element.io>
2025-11-21 15:25:21 +0000

    Improve docs

--- src/qr_login.rs
+++ src/qr_login.rs
@@ -6,7 +6,7 @@ use wasm_bindgen::prelude::*;
 
 use crate::vodozemac::Curve25519PublicKey;
 
-/// The intent of the QR code login.
+/// The intent of the device that generated/displayed the QR code.
 ///
 /// The QR code login mechanism supports both, the new device, as well as the
 /// existing device to display the QR code.
